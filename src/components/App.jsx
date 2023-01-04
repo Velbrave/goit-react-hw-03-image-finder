b805a29bd634b05457cc0bc7dd92632a0a9c9d7b
Vitalij Nesvyatipaska <Parkovaja60@gmail.com>
2023-01-04 12:55:24 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import ImageGallery from './ImageGallery/ImageGallery';
+import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+import Modal from './Modal/Modal';
+import Searchbar from './Searchbar/Searchbar';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Searchbar />
+      <ImageGallery />
+      <ImageGalleryItem />
+      <Modal />
     </div>
   );
 };
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,9 @@
+const ImageGallery = () => {
+  return (
+    <ul class="gallery">
+      <li></li>
+    </ul>
+  );
+};
+
+export default ImageGallery;
